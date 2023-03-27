ad16e15ea5433a7607c77871cb54d753b1f3086e
Neeeveess <jvneves_marq@hotmail.com>
2023-03-27 15:11:52 -0300

    mundando diretorio

---
+++ calculadora/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Calculator from './main/Calculator';
+
+function App() {
+  return (
+  <>
+    <h1>Calculadora</h1>
+    <Calculator/>
+  </>
+  );
+}
+
+export default App;
