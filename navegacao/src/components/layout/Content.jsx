b69ca8b217d442e81363ceed3c5523db64c499b9
Neeeveess <jvneves_marq@hotmail.com>
2023-04-03 15:21:00 -0300

    routes

--- navegacao/src/components/layout/Content.jsx
+++ navegacao/src/components/layout/Content.jsx
@@ -1,10 +1,22 @@
+import "./Content.css";
+
 import React from "react";
-import './Content.css'
+import { Routes, Route } from "react-router-dom";
 
-const Content = props =>(
-    <main className="Content">
-        Componente Content
-    </main>
-)
+import About from "./../../views/examples/About";
+import Home from "./../../views/examples/Home";
+import Param from './../../views/examples/Param';
+import NotFound from './../../views/examples/NotFound';
 
-export default Content
+const Content = (props) => (
+  <main className="Content">
+    <Routes>
+      <Route path="/" exact element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/param/:id" element={<Param />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </main>
+);
+
+export default Content;
--- navegacao/src/components/layout/Menu.jsx
+++ navegacao/src/components/layout/Menu.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 import './Menu.css'
 
+import { Link } from "react-router-dom";
+
 const Menu = props =>(
     <aside className="Menu">
-        Componente Menu
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">Sobre</Link>
+                </li>
+                <li>
+                    <Link to="/param/123">Param 01</Link>
+                </li>
+                <li>
+                    <Link to="/param/legal">Param 02</Link>
+                </li>
+                <li>
+                    <Link to="/naoexiste">Nao Existe</Link>
+                </li>
+            </ul>
+        </nav>
     </aside>
 )
 
